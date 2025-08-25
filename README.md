b26453cc7b556956232cb723edec582fbd70a6cc
sena <senadmrbsx@gmail.com>
2025-08-25 11:10:11 +0300

    Klasör yapısı düzeltildi

---
+++ README.md
@@ -0,0 +1,37 @@
+IP-Control-Automation
+Python (PyQt6) ve SQLite kullanılarak geliştirilmiş IP Yönetim ve Kontrol Otomasyonu. IP atama, rezerve etme ve yönetimi sağlayan bir masaüstü uygulama.
+
+IP Yönetim ve Kontrol Otomasyonu
+Bu proje, DATAHOME Yazılım Bilgi Teknolojileri için stajyerler olarak bir ekip çalışması kapsamında geliştirilmiştir.
+Amaç, Python (PyQt6) ile GUI (arayüz) ve SQLite ile veritabanı kullanarak bir IP Yönetim ve Kontrol Otomasyon Sistemi oluşturmaktır.
+
+📌 Projenin Amacı
+IP bloklarını (CIDR veya Range) yönetmek
+Kullanılabilir, rezerve edilmiş ve aktif IP’leri takip etmek
+IP’lerin kim tarafından eklendiğini ve kime atandığını kaydetmek
+Tek kullanıcı oturumuna izin vererek çakışmaları önlemek
+Yapılan tüm işlemleri loglamak
+Kullanılan Teknolojiler
+Python 3.x
+
+PyQt6 (Arayüz)
+
+SQLite (Veritabanı)
+
+Özellikler
+Kullanıcı girişi ve oturum yönetimi
+
+IP bloklarının eklenmesi ve rezerve edilmesi
+
+IP kullanım durumlarının takibi
+
+Yapılan tüm işlemlerin loglanması
+
+PyQt6 ile kullanıcı dostu arayüz
+
+Kurulum ve Çalıştırma
+Bu projeyi klonlayın: git clone https://github.com/kullaniciadi/ip-manager.git cd ip-manager
+Gerekli bağımlılıkları yükleyin: pip install -r requirements.txt
+Uygulamayı başlatın: python main.py
+Proje Başlangıç Tarihi
+21.08.2025
---
+++ database/database_setup.py
@@ -0,0 +1,85 @@
+#bu dosya çalıştırılınca ip_data.db dosyasında database dosyaları otomatik oluşur.
+import sqlite3
+import os
+
+#veritabanı yolunu belirtiriz
+DB_PATH = os.path.join(os.path.dirname(__file__), "ip_data.db")
+
+#veritabanını bağlar yoksa da oluşturucak
+conn = sqlite3.connect(DB_PATH)
+cursor = conn.cursor()
+
+#foreign key kullancağımız için özelliği açalım
+cursor.execute("PRAGMA foreign_keys= ON")
+
+#kullanıcı tabloları tasarımları, çalıştırmadan önce ekip olarak tartışılacak, foreign keyler eklenecek
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS User(
+               user_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+               username TEXT NOT NULL UNIQUE,
+               password TEXT NOT NULL,
+               name TEXT NOT NULL,
+               surname TEXT NOT NULL,
+               active_session INTEGER DEFAULT 0
+               )
+"""
+)
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Customer(
+               customer_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+               customer_name TEXT NOT NULL,
+               customer_surname TEXT NOT NULL
+               )
+"""
+)
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS IP_Blocks(
+               block_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+               user_ID INTEGER NOT NULL
+               block_name TEXT NOT NULL,
+               range_start TEXT,
+               range_end TEXT,
+               CIDR TEXT,
+               asno TEXT,
+               timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+               status TEXT CHECK(status IN('active','inactive')) NOT NULL DEFAULT 'active',
+               FOREIGN KEY (user_ID) REFERENCES User(user_ID)
+               )
+"""
+)
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS IP_Table (
+                IP_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                block_ID INTEGER,
+                edited_by_user_ID INTEGER,
+                customer_ID INTEGER,
+                IP_adress TEXT NOT NULL UNIQUE,
+                reservation TEXT,
+                note TEXT,
+                start_date DATE,
+                end_date DATE,
+                FOREIGN KEY (block_ID) REFERENCES IP_Blocks(block_ID),
+                FOREIGN KEY (edited_by_user_ID) REFERENCES User(user_ID),
+                FOREIGN KEY (customer_ID) REFERENCES Customers(customer_ID)
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Logs (
+    log_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_ID INTEGER,
+    action TEXT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_ID) REFERENCES User(user_ID)
+)
+""")
+
+# Kaydet ve kapat zorunlu
+conn.commit()
+conn.close()
+
+#deneme
+print("Tablolar başarıyla oluşturuldu!")
