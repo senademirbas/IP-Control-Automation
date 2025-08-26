2473c2b2f435deb95cc6c5b89d2690522da2bd91
Doğukan Bilal Örs <165324202+DogukanOrs@users.noreply.github.com>
2025-08-26 14:16:39 +0300

    oldusonunda

--- main.py
+++ main.py
@@ -8,13 +8,11 @@ db_path = os.path.join('database','ip_data.db')
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
-# User tablosundaki tüm verileri sil
-cursor.execute("DELETE FROM user")
 
 # Yeni bir kayıt ekle
 cursor.execute("""
-    INSERT INTO User (username, password,name,surname) VALUES (?, ?, ?, ?)
-""", ("kullanıcı1", "123456","Kullanıcı","Bir"))
+    INSERT INTO User (username, password,name,surname,active_session) VALUES (?, ?, ?, ?, ?)
+""", ("kullanıcı1", "123456","Kullanıcı","Bir",1))
 
 # Değişiklikleri kaydet ve bağlantıyı kapat
 conn.commit()
