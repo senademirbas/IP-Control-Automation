48224bb56e86fb5910e058da72e6e75da1984d72
Doğukan Bilal Örs <165324202+DogukanOrs@users.noreply.github.com>
2025-08-26 17:15:27 +0300

    tekrar pull alıcam

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ db_path = os.path.join('database','ip_data.db')
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
+cursor.execute("""
+    INSERT INTO User (username, password,name,surname,active_session) VALUES (?, ?, ?, ?, ?)
+""", ("kullanıcı1", "123456","Kullanıcı","Bir",1))
 
-# Yeni bir kayıt ekle
 cursor.execute("""
     INSERT INTO Customer (customer_name,customer_surname) VALUES (?, ?)
-""", ("musteri1", "2"))
+""", ("musteri2", "soyad2"))
 
 # Değişiklikleri kaydet ve bağlantıyı kapat
 conn.commit()
